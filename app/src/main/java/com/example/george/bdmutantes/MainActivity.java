3e0a46f613839d9fdac3cb4dd55ae03f5a77bac2
Felipe Moreno <felipemorenolopes@gmail.com>
2018-11-02 14:07:13 -0300

    Dashboard com os 4 botões funcionais criada.

--- app/src/main/java/com/example/george/bdmutantes/MainActivity.java
+++ app/src/main/java/com/example/george/bdmutantes/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.george.bdmutantes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void cadastrarMutante(View view){
+        Intent it = new Intent(this, CadastrarActivity.class);
+        startActivity(it);
+    }
+
+    public void listarMutante(View view){
+        Intent it = new Intent(this, ListarActivity.class);
+        startActivity(it);
+    }
+
+    public void pesquisarMutante(View view){
+        Intent it = new Intent(this, PesquisarActivity.class);
+        startActivity(it);
+    }
+
+    public void fecharApp(View view){
+        System.exit(0);
     }
 }
